e9a2603b2883165e929ae54fe05ef36dfb02e859
Vitor Gabriel <vitorg.34825@gmail.com>
2022-03-17 12:07:52 -0300

    alterando tela

--- window.py
+++ window.py
@@ -1,11 +1,21 @@
+from asyncio.windows_events import NULL
 from tkinter import *
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
 
 
 def btn_clicked():
-    print("Button Clicked")
+    if (entry0.get() == '' or entry1.get() == ''):
+        print('erro')
+        showinfo(title='Erro', message='Todos os campos precisam ser preenchidos')
+    else:
+        print(f"Operação Selecionada {entry0.get()}")
+        print(f"Bot Selecionado {entry1.get()}")
 
 
 window = Tk()
+window.title('RPA Deploy')
 
 window.geometry("1184x678")
 window.configure(bg = "#ffffff")
@@ -32,13 +42,36 @@ canvas.create_text(
 
 entry0_img = PhotoImage(file = f"img_textBox0.png")
 entry0_bg = canvas.create_image(
-    218.0, 301.5,
+    218.0, 299.5,
     image = entry0_img)
 
-entry0 = Entry(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
+
+def month_changed(event):
+    """ handle the month changed event """
+    showinfo(
+        title='Result',
+        message=f'You selected {selected_month.get()}!'
+    )
+    entry0['str'] = 'disable'
+
+selected_month = StringVar()
+
+entry0 = ttk.Combobox(window, 
+                            # bd = 0,
+                            # bg = "#ffffff",
+                            # highlightthickness = 0,
+                            font=("DoppioOne-Regular", int(16.0)),
+                            
+                            textvariable=selected_month)
+
+entry0['values'] = ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']
+
+# entry0.bind('<<ComboboxSelected>>', month_changed)
+# entry0.current(0)
+entry0['state'] = 'readonly'
+
+
+
 
 entry0.place(
     x = 33.5, y = 281,
@@ -46,20 +79,26 @@ entry0.place(
     height = 39)
 
 canvas.create_text(
-    197.5, 245.5,
+    197.5, 260.5,
     text = "Selecione a operação:",
     fill = "#000000",
     font = ("DoppioOne-Regular", int(19.0)))
 
 entry1_img = PhotoImage(file = f"img_textBox1.png")
 entry1_bg = canvas.create_image(
-    218.0, 410.5,
+    218.0, 408.5,
     image = entry1_img)
 
-entry1 = Entry(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
+entry1 = ttk.Combobox(window, 
+                            # bd = 0,
+                            # bg = "#ffffff",
+                            # highlightthickness = 0,
+                            font=("DoppioOne-Regular", int(16.0)),
+                            values=[
+                                    "01", 
+                                    "03"])
+
+entry1.set('03')
 
 entry1.place(
     x = 33.5, y = 390,
@@ -67,7 +106,7 @@ entry1.place(
     height = 39)
 
 canvas.create_text(
-    196.5, 354.5,
+    196.5, 370.5,
     text = "Selecione o BOT:",
     fill = "#000000",
     font = ("DoppioOne-Regular", int(19.0)))
