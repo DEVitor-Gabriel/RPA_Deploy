212f1504d22ec74e805e130c4f7b2d9d51f1bc38
Vitor <Gabriel>
2022-03-17 17:55:34 -0300

    multiplos deploy

--- deploy.py
+++ deploy.py
@@ -5,11 +5,12 @@ import shutil
 
 def deploy():
     orig = Path('//cvt-d589/c$/Users/UIPATH/Documents/UiPath')
-    dest = Path('//cvt-d293/c$/QAStransfer/UiPath')
-    print(orig)
-    print(dest)
+    dest1 = Path('//cvt-d293/c$/QAStransfer/UiPath')
+    dest2 = Path('//cvt-d293/c$/QAStransfer2/UiPath')
+    dests = [dest1, dest2]
 
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
-        
-    shutil.copytree(orig, dest)
+    for dest in dests:
+        if os.path.exists(dest):
+            shutil.rmtree(dest)
+            
+        shutil.copytree(orig, dest)
